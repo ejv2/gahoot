1fd8b6e9b52d86d1e4fadfcdd4eb69d32cddb270
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-06-04 01:00:09 +0100

    Add configuration parser
    More config keys to come once more is implemented

---
+++ config/conf.go
@@ -0,0 +1,36 @@
+// Package config represents an abstraction over the Gahoot config file
+// and implements a parser, loader and validator for said configuration.
+//
+// Configuration consists of a simple text file, which contains a set of
+// keys defined via "key: value", where value may be blank. Trailing
+// whitespace before and after the "value" will be stripped, but any other
+// valid UTF-8 characters may be otherwise
+package config
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator"
+)
+
+type Config struct {
+	validator *validator.Validate
+
+	ListenAddr string
+	ListenPort uint64
+}
+
+// FullAddr returns the full address for use in serving based on
+// both ListenAddr and ListenPort in the format expected by net.Dial
+func (c Config) FullAddr() string {
+	return fmt.Sprintf("%s:%d", c.ListenAddr, c.ListenPort)
+}
+
+func New(path string) (Config, error) {
+	c := Config{
+		validator: validator.New(),
+	}
+	err := parse(&c, path)
+
+	return c, err
+}
---
+++ config/parse.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Parsing errors
+var (
+	ErrInvalidKey = errors.New("config: invalid key")
+	ErrUnknownKey = errors.New("config: unknown key")
+	ErrParse      = errors.New("config: parse error")
+)
+
+// parse advances through the config file, extracting one config key
+// per line. Keys are separated from content by a single ':' (colon).
+// Any UTF-8 text can be placed around the colon and will be handled.
+func parse(c *Config, path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("open config failed: %w", err)
+	}
+
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanLines)
+	for num := 1; s.Scan(); num++ {
+		l := s.Text()
+		s := strings.SplitN(l, ":", 2)
+
+		// Probably blank line
+		if len(s) != 2 {
+			continue
+		}
+		// Comment; ignore
+		if strings.HasPrefix(l, "//") {
+			continue
+		}
+
+		var err error
+		key, trail := strings.ToLower(s[0]), strings.Trim(s[1], " \t")
+		switch key {
+		case "addr":
+			c.ListenAddr = trail
+		case "port":
+			c.ListenPort, err = strconv.ParseUint(trail, 10, 64)
+		default:
+			err = fmt.Errorf("unknown key: %q", key)
+		}
+
+		// Parsing error from last pass
+		if err != nil {
+			return fmt.Errorf("config: parse error: %w: line %d", err, num)
+		}
+	}
+
+	return nil
+}
