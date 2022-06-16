7af01f8871f59a643f170ffb47497165fb421a78
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-06-16 22:21:35 +0100

    Implement client-side FSM mechanism
    Client maintains a state interface (which is simply a JS version of a
    function pointer for our use case). This function is called when a
    message is received and returns a new state. This allows us to shift
    state based on what messages come in, which lets the client be entirely
    predictable in response to events.

--- frontend/src/play.ts
+++ frontend/src/play.ts
@@ -14,14 +14,38 @@ const PlayEndpoint = "ws://" + location.host + "/api/play/"
 let conn: WebSocket
 let plr: PlayerState
 
+// GameMessage represents a message received over the websocket channel
+interface GameMessage {
+    action: string
+    data: any
+}
+
+// State interface
+// Represents a single state in the client finite state machine
+//
+// Each time a message is received, the state is called and returns a new
+// state, which could be itself to remain in this state.
+//
+// This design is inspired by Rob Pike's 2010 talk Lexical Scanning in Go:
+// https://youtu.be/HxaD_trXwRE?t=740
+interface GameState {
+    (this: PlayerState, ev: GameMessage): GameState
+}
+
 // Set up alpine on the window
 // For debugging purposes
 window.Alpine = Alpine
 
-// PlayerState represents the current state of the page for the player.
-// It holds and manages the updating of dynamic data to be displayed by
-// the HTML engine.
+// PlayerState is the current datamodel for the client.
+//
+// Any code which mutates the state of the application based
+// on events from the server or anything else *must* be a member
+// of this class for the changes to be reflected in the DOM.
 class PlayerState {
+    private pin: number
+    private uid: number
+
+    private state: GameState
 
     connected: boolean
     points: number
@@ -34,11 +58,19 @@ class PlayerState {
     constructor() {
         this.connected = false
         this.points = this.rank = 0
+
+        this.pin = game
+        this.uid = user
+
+        this.state = function e(ev: GameMessage): GameState {
+                console.log(ev)
+                return this.state
+        }
     }
 
     // Initializes event listeners such that Alpine will track changes for us
     //
-    // Data should not be mutated in here, if possible
+    // Data should not be mutated in here, unless the mutation is *really* simple
     // Delegate to methods where appropriate
     init() {
         conn.onopen = () => {this.handleConnection(true)}
@@ -55,10 +87,16 @@ class PlayerState {
 
     // handleMsg is called when a websocket message arrives
     //
-    // Parsing should take place here, but actual mutation *must*
-    // take place in separate methods.
+    // This must *only* be used to do parsing and state shifts
+    // and must never mutate state itself
     handleMsg(ev: MessageEvent) {
-        console.log(ev)
+        let [action, ...rest]: string[] = ev.data.toString().split(" ")
+        let msg: GameMessage = {
+            action: action,
+            data: JSON.parse(rest.join(" "))
+        }
+
+        this.state = this.state(msg)
     }
 }
 
