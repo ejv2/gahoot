4f1521bc94fd1cbb98148e47d7d237c806570f75
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-06-16 22:28:01 +0100

    Implement message loop and PING system
    Player runner is now split into two halves: the read half and the write
    half. Both are controlled by the same context, which will cause both to
    exit on cancellation/timeout. The message loop will simply continually
    read messages from the user, but will timeout after the expected time
    within which a PING could arrive. This means that the server will expect
    keep-alive PINGs from the client within this time or the client will be
    considered to have lost connection (20s is the timeout). The PING system
    will send a ping control packet to the client every 15 seconds. It will
    not wait for a response: the read half will obtain the response.

--- game/player.go
+++ game/player.go
@@ -2,12 +2,31 @@ package game
 
 import (
 	"context"
-	"io"
 	"log"
-	"strings"
 	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+// WebSocket server message commands.
+// Each websocket message is formatted as "<verb> <arg0>...<arg n>", where args
+// are variadic and separated by whitespace. The "<verb>" can be any of these
+// constants.
+const (
+	CommandGameOver     = "end"
+	CommandQuestionOver = "qend"
+	CommandNewQuestion  = "ques"
+	CommandNewOptions   = "opts"
+	CommandSeeResults   = "res"
+)
+
+// WebSocket client message commands.
+// Client equivalent of server message commands. See documentation for server
+// message commands for more details on format.
+const (
+	MessageAcknowledge = "ack"
+	MessageIdenfity    = "ident"
+	MessageAnswer      = "ans"
 )
 
 // A Player is one registered player as part of a running game. Each player is
@@ -27,26 +46,58 @@ type Player struct {
 
 	Connected bool
 	Ctx       context.Context
+	Cancel    context.CancelFunc
 
-	lastPing time.Time
-	conn     *websocket.Conn
+	Send chan string
+	conn *websocket.Conn
 }
 
-func pingHandler() {
+func (p Player) writer(interval time.Duration) {
+	for {
+		select {
+		case msg := <-p.Send:
+			err := p.conn.WriteMessage(websocket.TextMessage, []byte(msg))
+			if err != nil {
+				p.Cancel()
+				return
+			}
+		case <-time.After(interval):
+			log.Println("sending ping message to", p.conn.RemoteAddr())
+			err := p.conn.WriteControl(websocket.PingMessage, nil, time.Now().Add(interval))
+			if err != nil {
+				p.Cancel()
+				return
+			}
+		case <-p.Ctx.Done():
+			return
 
+		}
+	}
 }
 
 // Run is the game runner thread. It continually receives from the "conn"
 // websocket connection until notified to stop by "ctx"
 func (p Player) Run(ev chan GameAction) {
 	bail := func(why string) {
+		log.Println("websocket: closing due to error:", why)
 		p.conn.WriteControl(websocket.CloseMessage,
 			websocket.FormatCloseMessage(websocket.CloseInvalidFramePayloadData, why),
-			time.Now().Add(time.Second))
+			time.Now().Add(time.Second*10))
 		p.conn.Close()
 	}
-	end, _ := p.Ctx.Deadline()
-	p.conn.SetReadDeadline(end)
+
+	lastPong := time.Now()
+	pongInterval, pongTimeout := time.Second*15, time.Second*4
+	p.conn.SetReadDeadline(lastPong.Add(pongInterval).Add(pongTimeout))
+	p.conn.SetPongHandler(func(appData string) error {
+		log.Println("got pong response with latency", time.Now().Add(-pongInterval).Sub(lastPong), "from", p.conn.RemoteAddr())
+		lastPong = time.Now()
+		p.conn.SetReadDeadline(lastPong.Add(pongInterval).Add(pongTimeout))
+		return nil
+	})
+
+	go p.writer(pongInterval)
+	defer p.Cancel()
 
 readloop:
 	for {
@@ -54,10 +105,12 @@ readloop:
 		switch {
 		case err != nil:
 			p.conn.Close()
-			break readloop
+			return
+		case t == websocket.PingMessage || t == websocket.PongMessage:
+			continue readloop
 		case t != websocket.TextMessage:
 			bail("expected text messages, got binary")
-			break readloop
+			return
 		}
 		msg := new(strings.Builder)
 		io.Copy(msg, r)
@@ -65,8 +118,16 @@ readloop:
 
 		select {
 		case <-p.Ctx.Done():
-			break
+			break readloop
 		default:
 		}
 	}
+
+	// If we got here, the game is done and the player instance is exiting
+	// Send the final goodbyes
+	p.Send <- CommandGameOver
+	p.conn.WriteControl(websocket.CloseMessage,
+		websocket.FormatCloseMessage(websocket.CloseNormalClosure, "game over"),
+		time.Now().Add(time.Second*10))
+	p.conn.Close()
 }
