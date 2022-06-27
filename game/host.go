0962b3e71e4781ffa9728d4cfea98476c62d9517
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-06-27 14:24:55 +0100

    Add skeleton runner for host
    Will be properly implemented later, when the rest of the game is up and
    running.

--- game/host.go
+++ game/host.go
@@ -1,7 +1,42 @@
 package game
 
+import "log"
+
 // The Host of a game is the client which receives incoming question texts and
 // which handles type synchronisation for the rest of the game.
 type Host struct {
 	Client
+}
+
+func (h Host) Run(ev chan GameAction) {
+	h.Open()
+	defer func() {
+		select {
+		case ev <- EndGame{"host disconnect", false}:
+		case <-h.Ctx.Done():
+		}
+
+		h.Cancel()
+		log.Printf("%s (host) disconnected", h.conn.RemoteAddr())
+	}()
+
+readloop:
+	for {
+		cmd, _, err := h.ReadString()
+		if err != nil {
+			log.Println("host:", err)
+			h.CloseReason(err.Error())
+			return
+		}
+
+		// TODO: Go through commands here
+		switch cmd {
+		}
+
+		select {
+		case <-h.Ctx.Done():
+			break readloop
+		default:
+		}
+	}
 }
