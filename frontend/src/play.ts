be466a5a49c326cce9858889bfd0c4684ef5e6ed
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-07-04 02:22:20 +0100

    Begin to implement client gameplay
    Implemented states for FSM.
    Implemented first few HTML state frontends.

--- frontend/src/play.ts
+++ frontend/src/play.ts
@@ -12,6 +12,34 @@ import Alpine from "alpinejs"
 let conn: WebSocket
 let plr: PlayerState
 
+// Possible game state IDs
+enum States {
+    Loading = 1,
+    Waiting,
+    Countdown,
+    Question,
+    Answer,
+    Finished
+}
+
+interface CountdownData {
+    length: number,
+    title: string,
+}
+
+interface QuestionData {
+    title: string,
+    image: string,
+    answers: string[]
+}
+
+interface FeedbackData {
+    correct: boolean,
+    points: number,
+    placement: number,
+    behind: string,
+}
+
 // Set up alpine on the window
 // For debugging purposes
 window.Alpine = Alpine
@@ -26,10 +54,19 @@ class PlayerState {
     private uid: number
 
     private state: common.GameState<PlayerState>
+    stateID: States
 
     connected: boolean
     points: number
     rank: number
+
+    countdown: CountdownData
+    countdownCount: number
+    private countdownHndl: number
+
+    question: QuestionData
+    feedback: FeedbackData
+    submitSpinner: boolean
 
     // Initializes data defaults
     //
@@ -42,10 +79,33 @@ class PlayerState {
         this.pin = game
         this.uid = user
 
-        this.state = function e(ev: common.GameMessage): common.GameState<PlayerState> {
-            console.log(ev)
-            return this.state
+        this.countdown = {
+            title: "",
+            length: 5,
         }
+        this.countdownCount = this.countdown.length
+        this.countdownHndl = 0
+
+        this.question = {
+            title: "Error!",
+            image: "https://developer.valvesoftware.com/w/images/5/5b/Missing_textures_example.png",
+            answers: [
+                "1",
+                "2",
+                "3",
+                "4"
+            ],
+        }
+        this.feedback = {
+            correct: false,
+            points: 0,
+            behind: "Gabe Newell",
+            placement: 3
+        }
+        this.submitSpinner = false
+
+        this.state = this.stateWaiting
+        this.stateID = States.Loading
     }
 
     // Initializes event listeners such that Alpine will track changes for us
@@ -65,15 +125,16 @@ class PlayerState {
         common.SendMessage(conn, "ident", this.uid)
         console.log("authenticated to game " + this.pin.toString())
         this.handleConnection(true)
+        this.stateID = States.Waiting
     }
 
     // handleConnection is called when a websocket connection changes state
     handleConnection(connected: boolean) {
         this.connected = connected
 
-        // TODO: Check here if current state was showing results.
-        // If it WAS NOT this state, redirect back to join page (host probably left).
-        // Else, do nothing and just terminate.
+        if (!this.connected && this.stateID != States.Finished) {
+            window.location.href = "/join";
+        }
     }
 
     // handleMsg is called when a websocket message arrives
@@ -88,6 +149,91 @@ class PlayerState {
         }
 
         this.state = this.state(msg)
+    }
+
+    startCountdown(ev: common.GameMessage) {
+        this.countdown = <CountdownData>ev.data;
+        if (this.countdown.length == 0) {
+            return this.state
+        }
+
+        this.countdownHndl = window.setInterval(() => {
+            this.countdownCount--
+            if (this.countdownCount == 0) {
+                clearInterval(this.countdownHndl)
+                common.SendMessage(conn, "ack", {})
+
+                this.state = this.stateQuestion
+            }
+        }, 1000)
+    }
+
+    // STATE FUNCTIONS
+    // ---------------
+
+    // Waiting for game to start
+    stateWaiting(ev: common.GameMessage): common.GameState<PlayerState> {
+        if (ev.action != "starting") {
+            console.warn("Expected starting message, got " + ev.action)
+            return this.stateWaiting
+        }
+
+        this.stateID = States.Countdown
+        this.startCountdown(ev)
+
+        return this.stateQuestion
+    }
+
+    // Show the question on screen
+    stateQuestion(ev: common.GameMessage): common.GameState<PlayerState> {
+        if (ev.action != "ques") {
+            console.warn("Expecting question, got " + ev.action)
+            return this.state
+        }
+
+        this.stateID = States.Question
+        this.question = <QuestionData>ev.data
+        return this.stateFeedback
+    }
+
+    // "Classroom genius?"
+    stateWaitingFeedback(ev: common.GameMessage): common.GameState<PlayerState> {
+        if (ev.action != "ansack") {
+            console.warn("Expecting answer acknowledge, got " + ev.action)
+            return this.state
+        }
+
+        return this.stateFeedback
+    }
+
+    // Showing if answer was correct or not
+    stateFeedback(ev: common.GameMessage): common.GameState<PlayerState> {
+        this.feedback = <FeedbackData>ev.data
+
+        this.points += this.feedback.points
+        this.rank = this.feedback.placement
+
+        switch (ev.action) {
+        case "quend":
+            break
+        case "res":
+            this.stateID = States.Finished
+            return this.stateEnding
+        default:
+            console.warn("Expecting results/feedback, got " + ev.action)
+            return this.state
+        }
+
+        this.stateID = States.Answer
+
+        this.startCountdown(ev)
+        return this.stateQuestion
+    }
+
+    // Put the FSM into an infinite loop - we are done
+    // Any further messages are ignored, including closes
+    stateEnding(ev: common.GameMessage): common.GameState<PlayerState> {
+        return this.stateEnding
     }
 }
 
